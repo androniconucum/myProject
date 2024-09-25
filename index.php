c111fc2ad37e3bbc31bd01787cee390d4052db4a
Andronico Nucum <nicounwntdmain@gmail.com>
2024-09-25 22:13:54 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<style>
+</style>
+<title>HOME</title>
+</head>
+<body class="bg-black mx-[150px] justify-center flex flex-col">
+    <div class="flex font-worksans font-medium align-middle text-center items-center ">
+    <ul class=" text-white flex gap-48">
+            <li class="text-white font-extrabold text-[2.5rem]">CS31A</li>
+            <div class="flex gap-14 align-middle text-center justify-center items-center">
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+            </div>            
+        </ul>
+        <div class="flex w-full justify-end">
+        <a href="login.php" class="text-black flex justify-end bg-[#E4E2DD] p-2 rounded-lg font-bold">Login Account</a>
+        </div>
+    </div>
+        <div class="flex my-2 justify-center">
+            <div class="bg-[#E4E2DD] w-[685px] font-worksans font-black h-[350px] px-[30px] rounded-lg mr-2">
+                <div class="flex justify-center mt-3">
+                <p class="text-[5rem] tracking-tighter leading-none">TYPOGRAPHY THAT DEFINES CREATIVITY</p>
+            </div>
+            <hr class="w-[37rem] bg-black pt-1 justify-start">
+            <div class="flex justify-end">
+            <p class="border w-36 mt-4 flex justify-center bg-black text-white font-medium p-2 text-[1rem] rounded-lg font-worksans">FONTS</p>
+            </div>
+            </div>
+            <!--2nd top box -->
+            <div class="rounded-lg border items-center flex">
+                <img src="images/CoverPage.svg" alt="" width="800px">
+            </div>
+        </div>
+         <!--2nd bottom box -->
+        <div class="flex">
+            <div class="w-[870px] rounded-lg px-[30px] flex items-start bg-black p-4 border">
+                <img src="images/giphy.webp" alt="">
+                <div class="pl-5 justify-center items-center">
+                    <p class="text-white font-worksans font-black text-[2.5rem] leading-snug">FOLLOW MY SOCIALS</p>
+                    <div class="flex gap-2 items-center pt-4">
+                <a href=""><img src="images/facebook.png" alt="" width="40px"></a>
+                <a href=""><img src="images/instagram.png" alt="" width="40px"></a>
+                <a href=""><img src="images/linkedin.png" alt="" width="40px"></a>
+                <a href=""><img src="images/twitter.png" alt="" width="40px"></a>
+                </div>
+                </div>
+            </div>
+
+            <div class="bg-[#E4E2DD] ml-2 w-[650px] rounded-lg px-[30px] font-worksans">
+                <div class="flex flex-col">
+                    <div class="flex gap-5 pb-4">
+                    <p class="font-worksans text-[4rem] font-black text-gray-500">4.50</p>
+                    <p class="font-worksans text-[4rem] font-black">Reviews</p>
+                    </div>
+                    <div class="flex gap-5 text-[1rem] font-worksans font-medium items-center">
+                    <p class="rounded-lg border-2 p-2 text-white bg-black">Roboto</p>
+                    <p class="rounded-lg border-2 p-2 text-white bg-black">Space Mono</p>
+                    <p class="rounded-lg border-2 p-2 text-white bg-black">Montserrat</p>
+                    <p class="rounded-lg border-2 p-2 text-white bg-black">Poppins</p>
+                </div>
+                <div class="flex gap-5 text-[1rem] font-worksans font-medium pt-2 items-center">
+                <p class="rounded-lg border-2 p-2 text-white bg-black">Inter</p>
+                <p class="rounded-lg border-2 p-2 text-white bg-black">Oswald</p>
+                <p class="rounded-lg border-2 p-2 text-white bg-black">Raleway</p>
+                <p class="rounded-lg border-2 p-2 text-white bg-black">Rubik</p>
+                <p class="rounded-lg border-2 p-2 text-white bg-black">Lora</p>
+                </div>
+                </div>
+                    </div>
+            </div>
+        </div>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*{html,php}"],
+  theme: {
+    fontFamily: {
+      worksans: ['Work Sans'],
+      rubikmonoone: ['Rubik Mono One'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
