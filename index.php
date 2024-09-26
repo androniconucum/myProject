9e2bff265d74cd8f23271bdc26689a368b34c58b
Andronico Nucum <nicounwntdmain@gmail.com>
2024-09-26 23:06:53 +0800

    responsive

--- index.php
+++ index.php
@@ -11,40 +11,40 @@
 </style>
 <title>HOME</title>
 </head>
-<body class="bg-black mx-[150px] justify-center flex flex-col">
+<body class="bg-black mx-[150px] md:mx-[60px] lg:mx-[100px] justify-center flex flex-col lg:my-3">
     <div class="flex font-worksans font-medium align-middle text-center items-center ">
-    <ul class=" text-white flex gap-48">
+    <ul class=" text-white flex gap-28">
             <li class="text-white font-extrabold text-[2.5rem]">CS31A</li>
-            <div class="flex gap-14 align-middle text-center justify-center items-center">
+            <div class="flex gap-10 align-middle text-center justify-center items-center">
             <li>ABOUT</li>
             <li>CONTACT</li>
             </div>            
         </ul>
         <div class="flex w-full justify-end">
-        <a href="login.php" class="text-black flex justify-end bg-[#E4E2DD] p-2 rounded-md font-bold">Login Account</a>
+        <a href="login.php" class="text-black flex justify-end bg-[#E4E2DD] p-2 rounded-md font-bold lg:p-1 lg:text-[1.2rem] md:text-[1rem] md:p-0">Login Account</a>
         </div>
     </div>
         <div class="flex my-2 justify-center">
-            <div class="bg-[#E4E2DD] w-[685px] font-worksans font-black h-[350px] px-[30px] rounded-md mr-2">
+            <div class="bg-[#E4E2DD] w-[685px] lg:w-[550px] md:w-[500px] font-worksans font-black h-[350px] lg:h-[280px] md:h-[300px] md:px-[30px] rounded-md mr-2">
                 <div class="flex justify-center mt-3">
-                <p class="text-[5rem] tracking-tighter leading-none">TYPOGRAPHY THAT DEFINES CREATIVITY</p>
+                <p class="text-[5rem] lg:text-[3.2rem] md:text-[3.5rem] tracking-tighter leading-none lg:leading-tight">TYPOGRAPHY THAT DEFINES CREATIVITY</p>
             </div>
-            <hr class="w-[37rem] bg-black pt-1 justify-start">
+            <hr class="w-[37rem] lg:w-[22rem] md:w-[22rem] bg-black pt-1 justify-start">
             <div class="flex justify-end">
             <p class="border w-36 mt-4 flex justify-center bg-black text-white font-medium p-2 text-[1rem] rounded-md font-worksans">FONTS</p>
             </div>
             </div>
             <!--2nd top box -->
             <div class="rounded-md border items-center flex">
-                <img src="images/CoverPage.svg" alt="" width="800px">
+                <img src="images/CoverPage.svg" alt="" class="lg:w-[550px] md:w-[500px]">
             </div>
         </div>
          <!--2nd bottom box -->
         <div class="flex">
-            <div class="w-[870px] rounded-md px-[30px] flex items-start bg-black p-4 border">
-                <img src="images/giphy.webp" alt="">
+            <div class="w-[870px] lg:w-[550px] md:w-[500px] rounded-md px-[30px] flex items-center bg-black p-4 border lg:h-[250px] md:h-[230px] lg:ml-1">
+                <img src="images/giphy.webp" alt="" class="lg:w-[240px] md:w-[200px]">
                 <div class="pl-5 justify-center items-center">
-                    <p class="text-white font-worksans font-black text-[2.5rem] leading-snug">FOLLOW MY SOCIALS</p>
+                    <p class="text-white font-worksans font-black text-[2.5rem] leading-snug lg:text-[1.7rem] lg:leading-7 md:text-[1.5rem] md:leading-8">FOLLOW MY SOCIALS</p>
                     <div class="flex gap-2 items-center pt-4">
                 <a href=""><img src="images/facebook.png" alt="" width="40px"></a>
                 <a href=""><img src="images/instagram.png" alt="" width="40px"></a>
@@ -54,19 +54,20 @@
                 </div>
             </div>
 
-            <div class="bg-[#E4E2DD] ml-2 w-[650px] rounded-md px-[30px] font-worksans">
-                <div class="flex flex-col">
-                    <div class="flex gap-5 pb-4">
-                    <p class="font-worksans text-[4rem] font-black text-gray-500">4.50</p>
-                    <p class="font-worksans text-[4rem] font-black">Reviews</p>
-                    </div>
-                    <div class="flex gap-4 text-[1rem] font-worksans font-medium items-center">
-                    <p class="rounded-md border-2 p-2 text-white bg-black">Roboto</p>
-                    <p class="rounded-md border-2 p-2 text-white bg-black">Space Mono</p>
+            <div class="bg-[#E4E2DD] ml-2 w-[650px] lg:w-[550px] md:w-[500px] rounded-md px-[30px] font-worksans">
+                <div class="flex flex-col lg:items-center justify-center">
+                    <div class="flex gap-5 ">
+                    <p class="font-worksans text-[4rem] font-black text-gray-500 lg:text-[3rem] md:text-[2.8rem]">4.50</p>
+                    <p class="font-worksans text-[4rem] font-black lg:text-[3rem] md:text-[2.8rem]">Reviews</p>
+                </div>
+                <hr class="w-[37rem] lg:w-[20rem] md:w-[23rem] bg-black pt-1 justify-center mb-5">
+                    <div class="flex gap-2 text-[1rem] font-worksans font-medium items-center lg:pt-3 md:pt-2">
+                    <p class="rounded-md border-2 p-2 text-white bg-black ">Roboto</p>
+                    <p class="rounded-md border-2 p-2 text-white bg-black">Mono</p>
                     <p class="rounded-md border-2 p-2 text-white bg-black">Montserrat</p>
                     <p class="rounded-md border-2 p-2 text-white bg-black">Poppins</p>
                 </div>
-                <div class="flex gap-4 text-[1rem] font-worksans font-medium pt-2 items-center">
+                <div class="flex gap-4 lg:gap-2 text-[1rem] font-worksans font-medium pt-2 items-center">
                 <p class="rounded-md border-2 p-2 text-white bg-black">Inter</p>
                 <p class="rounded-md border-2 p-2 text-white bg-black">Oswald</p>
                 <p class="rounded-md border-2 p-2 text-white bg-black">Raleway</p>
